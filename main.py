dbe0078bdacf0daf43a2e713b9bb0901c2a37fd7
Nadia Matin <nadiamatin@Nadias-Air.lan>
2022-08-14 18:00:48 -0500

    set up python project

---
+++ main.py
@@ -0,0 +1,13 @@
+#
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hello World")  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
